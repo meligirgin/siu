4a1c2f2ac376fbd3c4cf2293cf8c041a91a2ab50
Esra <nazifeesra98@gmail.com>
2022-10-07 14:52:36 +0300

    responsability

--- js/custom.carousel.js
+++ js/custom.carousel.js
@@ -1,23 +1,98 @@
-var owl = $('.owl-carousel-rtl');
-owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:4000,
-    autoplayHoverPause:false,
-    rtl:true
-});
-var owl = $('.owl-carousel-ltr');
-owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:4000,
-    autoplayHoverPause:false,
-    rtl:false
-});
+//index page caraousel
+var cartoonSlider = function(){  
+    let width = screen.width;
+    if(width>1500){
+        var owl = $('.owl-carousel-rtl');
+        owl.owlCarousel({
+            items:7,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:true
+        });
+        var owl = $('.owl-carousel-ltr');
+        owl.owlCarousel({
+            items:7,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:false
+        });
+    }else if(width<1500 && width>1000){
+        var owl = $('.owl-carousel-rtl');
+        owl.owlCarousel({
+            items:4,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:true
+        });
+        var owl = $('.owl-carousel-ltr');
+        owl.owlCarousel({
+            items:4,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:false
+        });
+    }else if(width<1000 && width>500){
+        var owl = $('.owl-carousel-rtl');
+        owl.owlCarousel({
+            items:3,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:true
+        });
+        var owl = $('.owl-carousel-ltr');
+        owl.owlCarousel({
+            items:3,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:false
+        });
+    }
+    else{
+        var owl = $('.owl-carousel-rtl');
+        owl.owlCarousel({
+            items:1,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:true
+        });
+        var owl = $('.owl-carousel-ltr');
+        owl.owlCarousel({
+            items:1,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:false,
+            rtl:false
+        });
+    }
+}
+cartoonSlider();
+window.addEventListener('resize', function(event) {
+    cartoonSlider();
+}, true);
+// gallery click function
 $( ".owl-carousel-rtl" ).click(function() {
     location.href = "gallery.html";
 });
